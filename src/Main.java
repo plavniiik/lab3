f4effdcfdc347b67e7460db07485e5ff9e2a6d3a
plavniiik <daryalavnikovich387@gmail.com>
2022-10-23 17:21:01 +0300

    new branch

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) throws Exception{
         System.out.println("Даша");
         System.out.println("Наташа");
+        System.out.println("Сергей");
     }
 }
